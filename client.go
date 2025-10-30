3368c194ba9ddc987cd8c9581fdad532c5a7bc0e
roaa <roaa.alaa222@gmail.com>
2025-10-31 00:10:10 +0200

    Initial commit - simple chatroom using RPC

---
+++ client.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net/rpc"
+	"os"
+	"strings"
+)
+
+func main() {
+	client, err := rpc.Dial("tcp", "127.0.0.1:1234")
+
+	if err != nil {
+		fmt.Println("Error connecting to server:", err)
+		return
+	}
+	defer client.Close()
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Connected to Chatroom. Type 'exit' to go.\n")
+
+	for {
+		fmt.Print("You: ")
+		text, _ := reader.ReadString('\n')
+		text = strings.TrimSpace(text)
+
+		if text == "exit" {
+			fmt.Println("Exiting chatroom, Bye :)")
+			break
+		}
+
+		var history []string
+
+		err := client.Call("ChatService.SendMessage", text, &history)
+		if err != nil {
+			fmt.Println("Error:", err)
+			break
+		}
+
+		fmt.Println("---- Chat History ----")
+		for _, msg := range history {
+			fmt.Print(msg)
+		}
+		fmt.Println("----------------------")
+
+	}
+}
---
+++ server.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"net/rpc"
+)
+
+type ChatService struct {
+	messages []string
+}
+
+func (c *ChatService) SendMessage(msg string, reply *[]string) error {
+	msg = msg + "\n"
+	c.messages = append(c.messages, msg)
+	*reply = c.messages
+	fmt.Printf("Received: %s\n", msg)
+	return nil
+}
+
+func main() {
+	server := new(ChatService)
+	rpc.Register(server)
+
+	listener, err := net.Listen("tcp", "127.0.0.1:1234")
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+
+	fmt.Println("Server running on port 1234")
+
+	if err != nil {
+		fmt.Println("Error registering service:", err)
+		return
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Connection error:", err)
+			continue
+		}
+		go rpc.ServeConn(conn)
+	}
+}
